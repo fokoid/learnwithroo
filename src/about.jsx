a3526d630aeeedfc5d1f9a7331670e472da4593d
Alex Thorne <alex@thorne.cc>
2018-01-21 06:37:26 +0000

    Add multiple React routes

---
+++ src/about.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => (
+  <div style={{margin:"20px"}}>
+    <h3><b>L</b>earn. <b>R</b>eact. <b>U</b>nderstand.</h3>
+  </div>
+)
+
+export default About
--- src/file-selector.jsx
+++ src/file-selector.jsx
@@ -2,23 +2,16 @@ import React from 'react'
 import Dropzone from 'react-dropzone'
 
 const FileSelector = ({callback}) => (
-  <div class="row">
-    <div class="column">
-      <div style={{margin:"20px"}}><h3> <b>L</b>earn. <b>R</b>eact. <b>U</b>nderstand. </h3></div>
-      <div>
-      </div>
-    </div>
-    <div class="column">
-      <div style={{margin:"20px"}}><h4> Select your image source for the game: </h4></div>
-      <div id='input'>
-        <button id="photolib">
-          Curated Collections
-        </button>
-        <p>OR</p>
-        <Dropzone id='uploader' accept='image/*' onDrop={callback}>
-          <p>Drop images here, or click to select images.</p>
-        </Dropzone>
-      </div>
+  <div>
+    <div style={{margin:"20px"}}><h4> Select your image source for the game: </h4></div>
+    <div id='input'>
+      <button id="photolib">
+        Curated Collections
+      </button>
+      <p>OR</p>
+      <Dropzone id='uploader' accept='image/*' onDrop={callback}>
+        <p>Drop images here, or click to select images.</p>
+      </Dropzone>
     </div>
   </div>
 )
