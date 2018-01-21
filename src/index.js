2d14f00635fbf106a82176d0836e576ebbe2f919
Alex Thorne <alex@thorne.cc>
2018-01-21 05:11:18 +0000

    Set up Firebase deployment

--- src/index.js
+++ src/index.js
@@ -4,6 +4,5 @@ import './index.css';
 import App from './App.jsx';
 import registerServiceWorker from './registerServiceWorker';
 
-document.title = 'LRU – Learn, React, Understand'
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
