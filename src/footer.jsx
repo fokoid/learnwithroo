d476a6c989490e5429fbbdaa6b0257d16ee1c710
Alex Thorne <alex@thorne.cc>
2018-01-22 16:00:50 +0000

    Rename to _learnwithroo_

--- src/footer.jsx
+++ src/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => (
   <footer>
     This application is under active development and a <a href='/disclaimer'>disclaimer</a> applies.
-    The source is available on <a href='https://github.com/thornecc/learn-react-understand'>GitHub</a>.
+    The source is available on <a href='https://github.com/thornecc/learnwithroo'>GitHub</a>.
   </footer>
 )
 
